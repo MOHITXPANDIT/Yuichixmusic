ec6f8839026abefc1b8fcbc22d91da028afb123e
MOHIT X PANDIT <87252563+MOHITXPANDIT@users.noreply.github.com>
2022-03-11 16:03:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,8 +23,8 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 UPSTREAM_REPO = "https://github.com/TechShreyash/SiestaXMusic"
 UPSTREAM_BRANCH = "main"
 
-SUPPORT_CHANNEL = "https://t.me/TechZBots"
-SUPPORT_GROUP = "https://t.me/TechZBots_Support"
+SUPPORT_CHANNEL = "https://t.me/Kristina_News"
+SUPPORT_GROUP = "https://t.me/KristinaSupportChat"
 
 THUMBNAIL = getenv("THUMB_LINK") 
 
